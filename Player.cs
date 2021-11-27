473a6c1740049e6a2044067ff3853861ca7d5a18
dereksigler <dereksigler@yahoo.com>
2021-11-26 23:25:18 -0600

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Fun_Game___Probably_Not
+{
+    class Player
+    {
+        public int hp = 100;
+        public int mana = 100;
+        public int strength = 10;
+
+        public static Timer timer = new Timer(5000);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+namespace Fun_Game___Probably_Not
+{
+    class Program
+    {
+        public static Player playerA = new Player();
+        public static Timer gametimer = new Timer(1000);
+        public static List<Player> monster = new List<Player>();
+        public static Map map = new Map();
+        public static void Main()
+        {
+            gametimer.Elapsed += MyElapsedMethod;
+            gametimer.AutoReset = true;
+            gametimer.Enabled = true;
+            gametimer.Start();
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            map.MapGenerate();
+            while (Console.Read() != 'q')
+            {
+                if (Console.Read() == 'l')
+                {
+                    locationmap();
+                }
+            }
+
+        }
+
+        // Specify what you want to happen when the Elapsed event is raised.
+        private static void MyElapsedMethod(object source, ElapsedEventArgs e)
+        {
+            
+            playerA.hp = playerA.hp - 2;
+ //           Console.WriteLine("HP left :{0}   MonsterCount:{1}", playerA.hp, monster.Count);
+            //create random number
+            //if matches 1 or something generate monster  (add monster)
+            var rand = new Random();
+            Console.SetCursorPosition(0, 0);
+            Console.Write("████████████████████████");
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("Random : {0}\t Monster Count:{1}", rand.Next(15), monster.Count);
+            if (monster.Count < 10)
+            {
+
+
+                if (rand.Next(15) == 1 )
+                {
+//                    Console.WriteLine("Oh no! A monster appeared!");
+                    Player tempMonster = new Player();
+                    monster.Add(tempMonster);
+
+                }
+            }
+
+        }
+        private static void drawScreen ()
+        {
+            Console.Clear();
+            Console.WriteLine("HP left :{0}   MonsterCount:{1}", playerA.hp, monster.Count);
+            for(int y=0; y < 50; y++) { 
+                for (int x=0;x<200; x++)
+                { Console.Write(" "); }
+                Console.WriteLine(" ");                
+               }
+        }
+
+        private static void locationmap()
+        {
+            int x= 0;
+            int y =0;
+            char tile;
+            gametimer.Stop();
+            Console.SetCursorPosition(0, 0);
+            Console.Write("████████████████████████");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("X:");
+        //    x = (Int32.Parse(Console.ReadLine()));
+            Console.Write("████████████████████████");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Y:");
+       //     y = (Int32.Parse(Console.ReadLine()));
+            Console.Write("████████████████████████");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("Checking location[{0},{1}]", x, y);
+            gametimer.Start();
+
+        }
+
+    }
+}

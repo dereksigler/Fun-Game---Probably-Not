8baf94bd372c2407dbe74591d59ebd41ff8c2b86
Harlan <Harlan.Sigler@gmail.com>
2021-11-28 12:35:40 -0600

    Create Monster.cs

---
+++ Monster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun_Game___Probably_Not
+{
+    class Monster
+    {
+
+    }
+}
